895d63d4ed2c5269102d48c95dad16c8fb2a1e7d
alexisayiku <alexisayiku6@gmail.com>
2023-07-13 11:10:11 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59; /* Length of the quote */
+    write(STDERR_FILENO, quote, len);
+    return (1);
+}
