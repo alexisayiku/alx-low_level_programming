52d5a4c065762377600110e1d05bdbe134abc9ba
alexisayiku <alexisayiku6@gmail.com>
2023-07-31 04:35:43 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
-#include "alx.h"
 #include <stdio.h>
 /**
-*  * main - check the code for Alx School students.
-*   *
-*    * Return: Always 0.
-*     */
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 char s1[98] = "Hello ";
@@ -12,7 +11,11 @@ char s2[] = "World!\n";
 char *p;
 printf("%s\n", s1);
 printf("%s", s2);
-p = _strcat(s1, s2);
+p = _strncat(s1, s2, 1);
+printf("%s\n", s1);
+printf("%s", s2);
+printf("%s\n", p);
+p = _strncat(s1, s2, 1024);
 printf("%s", s1);
 printf("%s", s2);
 printf("%s", p);
