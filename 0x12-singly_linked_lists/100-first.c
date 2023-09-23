e513e77b3d803a651356a0ef4c6f106ab48acd70
alexisayiku <alexisayiku6@gmail.com>
2023-09-23 03:04:15 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+* printBefore - this is a free memory of all list.
+* void: this is a pointer to head in fucntion
+* Return: Always success.
+*/
+void printBefore(void) __attribute__ ((constructor));
+void printBefore(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
